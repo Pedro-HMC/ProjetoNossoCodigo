91b47c2abb13c133f216716b027c1bd3851f75d5
Pedro Henrique Medeiros Câmara <pedro.henrique.camara2002@gmail.com>
2020-08-10 16:19:24 -0300

    Projeto Nosso Codigo

---
+++ proj-NossoCodigo/src/main/java/com/undefined/projetoNossoCodigo/ProjetoNossoCodigoApplication.java
@@ -0,0 +1,13 @@
+package com.undefined.projetoNossoCodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoNossoCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoNossoCodigoApplication.class, args);
+	}
+
+}
---
+++ proj-NossoCodigo/src/test/java/com/undefined/projetoNossoCodigo/ProjetoNossoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undefined.projetoNossoCodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoNossoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
