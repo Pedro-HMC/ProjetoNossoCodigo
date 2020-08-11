f18bb4360dc581cd4b72da7858ce8d4e19dba316
Nicole Paes <nicoleferreirapaes@gmail.com>
2020-08-11 19:09:33 -0300

    Pagina Inicial1

--- proj-NossoCodigo/src/main/java/com/undefined/projetoNossoCodigo/PaginainicialController.java
+++ proj-NossoCodigo/src/main/java/com/undefined/projetoNossoCodigo/PaginainicialController.java
@@ -1,9 +1,6 @@
 package com.undefined.projetoNossoCodigo;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -14,15 +11,5 @@ public class PaginainicialController {
     public String paginainicial(){
         return "/Paginainicial";
     }
-    @Configuration
-    @EnableWebSecurity
-    public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity security) throws Exception
-    {
-     security.httpBasic().disable();
-     security.csrf().disable();
-    }
-  }
 }
