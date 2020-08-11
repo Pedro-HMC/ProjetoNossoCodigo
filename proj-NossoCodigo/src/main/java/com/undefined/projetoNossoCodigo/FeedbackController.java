5ca4aad35535fe3d635be7d3da7f922eeaae774f
Pedro Henrique Medeiros Câmara <pedro.henrique.camara2002@gmail.com>
2020-08-11 01:17:52 -0300

    Feedback e Códigos

--- proj-NossoCodigo/src/main/java/com/undefined/projetoNossoCodigo/FeedbackController.java
+++ proj-NossoCodigo/src/main/java/com/undefined/projetoNossoCodigo/FeedbackController.java
@@ -2,10 +2,6 @@ package com.undefined.projetoNossoCodigo;
 
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,17 +64,5 @@ public class FeedbackController {
         }
           return "updateResposta";
         }
-    
-    @Configuration
-    @EnableWebSecurity
-    public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity security) throws Exception
-    {
-     security.httpBasic().disable();
-     security.csrf().disable();
-    }
-}
 
 }
